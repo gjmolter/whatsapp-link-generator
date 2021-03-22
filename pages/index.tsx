9b0439ab05059c0d0cf2b91c67e3a7f19fb22daa
Gabriel Molter <gjmolter.1997@gmail.com>
2021-03-22 18:22:51 -0300

    added GA and Amazon Ads

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   const [intro, setIntro] = useState("Hi! I'm interested in this product:");
 
   useEffect(() => {
-    var newUrl = `https://api.whatsapp.com/send/?phone=${encodeURIComponent(phone)}&text=${encodeURIComponent(
-      intro
-    )}%0A%0A${encodeURIComponent(product)}`;
+    var newUrl = `https://api.whatsapp.com/send/?phone=${encodeURIComponent(
+      phone
+    )}&text=${encodeURIComponent(intro)}%0A%0A${encodeURIComponent(product)}`;
     setUrl(newUrl);
   }, [intro, product, phone]);
 
@@ -25,12 +25,36 @@ export default function Home() {
           content="Simple URL Generation tool for WhatsApp Based Stores"
         />
         <title>WhatsApp Message URL Generator</title>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-H2C55SQ6FQ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag()
+              {dataLayer.push(arguments)}
+              gtag('js', new Date()); gtag('config', 'G-H2C55SQ6FQ');`,
+          }}
+        />
       </Head>
 
       <div className="container">
+        <div className="amazonDesktop">
+          <iframe
+            src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=11&l=ez&f=ifr&linkID=e4b5008e78779fd6d6222e3f606c5dc7&t=ivetravelledw-20&tracking_id=ivetravelledw-20"
+            width="120"
+            height="600"
+            scrolling="no"
+            style={{ border: "none", margin: 0, border: "none" }}
+          ></iframe>
+        </div>
         <div className="mainContent">
           <h1>WhatsApp Link Generator</h1>
-          <label htmlFor="link">Phone number<br/>(with country code - just numbers)</label>
+          <label htmlFor="link">
+            Phone number
+            <br />
+            (with country code - just numbers)
+          </label>
           <input
             type="text"
             value={phone}
@@ -64,6 +88,25 @@ export default function Home() {
             Copy Link
           </button>
           <span style={{ opacity: msg != "" ? 1 : 0 }}>{msg}</span>
+
+          <div className="amazonMobile">
+            <iframe
+              src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=9&l=ez&f=ifr&linkID=2a830ba594ded132f2242c8c29a7e244&t=ivetravelledw-20&tracking_id=ivetravelledw-20"
+              width="180"
+              height="150"
+              scrolling="no"
+              style={{ border: "none", margin: 0, border: "none" }}
+            ></iframe>
+          </div>
+        </div>
+        <div className="amazonDesktop">
+          <iframe
+            src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=11&l=ez&f=ifr&linkID=e4b5008e78779fd6d6222e3f606c5dc7&t=ivetravelledw-20&tracking_id=ivetravelledw-20"
+            width="120"
+            height="600"
+            scrolling="no"
+            style={{ border: "none", margin: 0, border: "none" }}
+          ></iframe>
         </div>
       </div>
     </div>
